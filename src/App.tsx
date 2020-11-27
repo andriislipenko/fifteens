af338f594ef47c0d4ba0031b0dec3a3b2baf673b
andriislipenko <andrii.slipenko@gmail.com>
2020-11-27 13:00:14 +0200

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Fifteens from './fifteens/fifteens';
 
 export class App extends React.Component {
     public render(): JSX.Element { 
-        return (<h1>Hello</h1>);
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/fifteens">
+                        <Fifteens/>
+                    </Route>
+                    <Redirect from="*" to="/fifteens"></Redirect>
+                </Switch>
+            </BrowserRouter>
+        );
     }
 }
---
+++ src/fifteens/fifteens.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Fifteens extends React.Component {
+    public render(): JSX.Element {
+        return (<h1>Hello</h1>);
+    }
+}
+
+export default Fifteens;
